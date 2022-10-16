3bf89e09f0f4ed46ce141c93e1ebd50698334eeb
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-10-17 00:21:13 +0200

    Add files via upload

---
+++ ponavljanje2.4/ponavljanje2.4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje2._4
+{
+    class Biljka
+    {
+
+    }
+    class Stablo : Biljka
+    {
+        bool opadajuListovi;
+        public Stablo(bool opadajuListovi)
+        {
+            this.opadajuListovi = opadajuListovi;
+        }
+
+        public bool OpadajuListovi { get => opadajuListovi; set => opadajuListovi = value; }
+    }
+    class Cvijet : Biljka
+    {
+    }
+    class Bijelogoricno : Stablo
+    {
+        public Bijelogoricno(bool opadajuListovi) : base(opadajuListovi)
+        {
+        }
+    }
+    class Crnogoricno : Stablo
+    {
+        public Crnogoricno(bool opadajuListovi) : base(opadajuListovi)
+        {
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bijelogoricno Hrast = new Bijelogoricno(true);
+            Crnogoricno Bor = new Crnogoricno(false);
+            Console.WriteLine(Hrast.OpadajuListovi);
+            Console.WriteLine(Bor.OpadajuListovi);
+            Console.ReadKey();
+        }
+    }
+}
